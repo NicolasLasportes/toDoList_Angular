c1872304a274f8698c332a2895d715328df32cd6
nicolas <nicolas.lasportes@gmail.com>
2018-12-04 19:07:31 +0100

    if checked list.done value change

--- src/app/list/list.component.ts
+++ src/app/list/list.component.ts
@@ -1,15 +1,38 @@
 import { Component, OnInit } from '@angular/core';
 import { list } from '../mock-list';
+import { List } from '../list';
+
 @Component({
   selector: 'app-list',
   templateUrl: './list.component.html',
   styleUrls: ['./list.component.css']
 })
+
 export class ListComponent implements OnInit {
   List = list;
+  selectedItem: List;
   constructor() { }
-
+  
   ngOnInit() {
   }
+  
+  onSelect(list: List): void {
+    //console.log(this.selectedItem);
+    this.selectedItem = list;
+  }
 
+  doneOrNot(item: List){
+    if(item.done == true)
+    {
+      item.done = false;
+    }
+    else
+    {
+      item.done = true;
+    }
+  }
+
+  // checked(list: List): void {
+  //   this.
+  // }
 }
--- src/app/mock-list.ts
+++ src/app/mock-list.ts
@@ -1,14 +1,14 @@
 import { List } from './list';
 
 export const list: List[] = [
-  { id: 1, name: 'item 1' },
-  { id: 2, name: 'item 2' },
-  { id: 3, name: 'item 3' },
-  { id: 4, name: 'item 4' },
-  { id: 5, name: 'item 5' },
-  { id: 6, name: 'item 6' },
-  { id: 7, name: 'item 7' },
-  { id: 8, name: 'item 8' },
-  { id: 9, name: 'item 9' },
-  { id: 10, name: 'item 10' }
+  { id: 1, name: 'item 1', done: true },
+  { id: 2, name: 'item 2', done: false },
+  { id: 3, name: 'item 3', done: true },
+  { id: 4, name: 'item 4', done: false },
+  { id: 5, name: 'item 5', done: true },
+  { id: 6, name: 'item 6', done: false },
+  { id: 7, name: 'item 7', done: true },
+  { id: 8, name: 'item 8', done: false },
+  { id: 9, name: 'item 9', done: true },
+  { id: 10, name: 'item 10', done: true }
 ];
